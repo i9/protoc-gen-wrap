611baa1242010c45a178d2a362192ed5fe862b2c
Junda Liu <junda@celer.network>
2021-04-18 23:03:05 -0700

    move opts.proto to separate folder

---
+++ example/run.sh
@@ -0,0 +1,1 @@
+protoc -I. -I../opts -I/usr/local/include --wrap_out=. --wrap_opt=paths=source_relative example.proto
--- main.go
+++ main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"flag"
+
+	"google.golang.org/protobuf/compiler/protogen"
+)
+
+func main() {
+	var flags flag.FlagSet
+	// can add options eg. fn = flags.String("file", "xxx.go", "name of generated go file")
+	protogen.Options{
+		ParamFunc: flags.Set,
+	}.Run(mygen)
+}
